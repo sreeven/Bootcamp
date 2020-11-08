1c26281f2acf6273ce380a9b03a84cbd24e18480
Sriven Ankam <sreeven@Srivens-MacBook-Pro.local>
2020-11-08 14:30:45 -0800

    query 1

--- sql-challenge/EmployeeSQL/Analysis.sql
+++ sql-challenge/EmployeeSQL/Analysis.sql
@@ -1,8 +1,10 @@
 -- #### Data Analysis
 
--- Once you have a complete database, do the following:
-
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e
+JOIN salaries AS s ON e.emp_no = s.emp_no;
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 
