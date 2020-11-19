bfeef2315bd1650b0c095a62850edf3afb268d01
Sriven Ankam <sreeven@Srivens-MacBook-Pro.local>
2020-11-18 23:03:46 -0800

    set base and flask

--- sqlalchemy-challenge/app.py
+++ sqlalchemy-challenge/app.py
@@ -1,3 +1,5 @@
+# Import Dependencies
+
 import numpy as np
 import datetime as dt
 
@@ -6,4 +8,15 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+# Set Base
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Flask
+
 from flask import Flask, jsonify
+
+app = Flask(__name__)
+
