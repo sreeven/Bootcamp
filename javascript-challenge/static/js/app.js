1ad3a887d6df3311b6a16e0457d09cb3486cb6f1
Sriven Ankam <sreeven@Srivens-MacBook-Pro.local>
2021-01-01 21:02:29 -0800

    clean

--- javascript-challenge/static/js/app.js
+++ javascript-challenge/static/js/app.js
@@ -1,23 +1,19 @@
 // from data.js
 var tableData = data;
 
-
 // table
 var body = d3.select("tbody");
-
 
 // build the table
 
 function buildTable(data) {
 
     // add row
-
     data.forEach((row) => {
         tRow = body.append("tr");
         tRow.text(Object.datetime);
 
         // add column
-
         Object.values(row).forEach((value) => {
             cell = tRow.append("td");
             cell.text(value);
@@ -31,15 +27,3 @@ function buildTable(data) {
 buildTable(data);
 
 
-// function buildTable(data) {
-//     // First, clear out any existing data
-//     tbody.html("");
-  
-//     // Next, loop through each object in the data
-//     // and append a row and cells for each value in the row
-//     data.forEach((dataRow) => {
-//       // Append a row to the table body
-//       const row = tbody.append("tr");
-
-//     });
-//   }
